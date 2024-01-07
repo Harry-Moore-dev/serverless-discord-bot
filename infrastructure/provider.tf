a2d55c10faa1b1e55d3a57c3b10a9fbca5461c17
Harry Moore <harryimoore18@googlemail.com>
2024-01-07 09:13:32 +0000

    add dependabot

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -7,7 +7,7 @@ terraform {
     }
     discord-application = {
       source  = "MichaelFraser99/discord-application"
-      version = "0.2.2"
+      version = "0.2.3"
     }
     terracurl = {
       source  = "devops-rob/terracurl"
